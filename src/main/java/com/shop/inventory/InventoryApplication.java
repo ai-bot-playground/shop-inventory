40d935096777243f2302a31b6c90928bb7a3cfa4
Mandervain <kmnski.l@gmail.com>
2026-06-29 17:12:48 +0200

    feat: add startup logging to all service Application classes

--- src/main/java/com/shop/inventory/InventoryApplication.java
+++ src/main/java/com/shop/inventory/InventoryApplication.java
@@ -1,12 +1,17 @@
 package com.shop.inventory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventoryApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(InventoryApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(InventoryApplication.class, args);
+        log.info("shop-inventory service started and ready to serve requests");
     }
 }
